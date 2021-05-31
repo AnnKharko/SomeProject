29b77272b246e8209b5567736c43173b722266b2
Ann <kharko2112.mechmat@gmail.com>
2021-05-31 14:14:23 +0300

    logs+

--- dataBase/models/Log.js
+++ dataBase/models/Log.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 const logSchema = new Schema({
     event: { type: String, required: true },
     realtorId: { type: String, required: true },
-    data: { type: Schema.Types.Mixed, required: true },
+    data: { type: Schema.Types.Mixed },
 }, { timestamps: true });
 
 module.exports = model('Log', logSchema);
